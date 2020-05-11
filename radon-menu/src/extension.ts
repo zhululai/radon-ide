b831742b4ae5b061ace5e9f8dce7a3e37395e230
sdagostini <stefania.dagostini@eng.it>
2020-05-11 17:29:56 +0200

    Updated extension code

--- radon-menu/src/extension.ts
+++ radon-menu/src/extension.ts
@@ -48,17 +48,55 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Defect Prediction Tool started');
 	});
 
-	let disposableDeployCSAR = vscode.commands.registerCommand('radon-menu.deployCSAR', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposableDeployCSAR = vscode.commands.registerCommand('radon-menu.deployCSAR', (uri:vscode.Uri) => {
+	// The code you place here will be executed every time your command is executed
 
-		vscode.window.showInformationMessage('Deployment process started');
+	//const simpleGitPromise = require('simple-git/promise')("projects/radon-csars");
+	//let path= e.fsPath();
+	//pushCsarToGitHubRepository();
+	const repo="github.com/radon-h2020/radon-csars.git";
+	// User name and password of your GitHub
+	const userName = 'radon-dev';
+	const password = 'radon-h2020';
+
+	//const simpleGitPromise = require('simple-git/promise')();
+	
+	// Set up GitHub url like this so no manual entry of user pass needed
+	//const gitHubUrl = 'https://${userName}:${password}@${repo}';
+	//simpleGitPromise.addRemote('origin',gitHubUrl);
+	var path = require('path')
+	var filePath=uri.fsPath;
+	var fileName=path.parse(filePath).base;
+	
+	var mkdirp = require('mkdirp');
+    var repoPath = 'd:/test/projects/radon-csars';
+	mkdirp.sync(repoPath);
+	
+	require('simple-git/promise')(repoPath).addRemote('origin', 'https://${userName}:${password}@github.com/radon-h2020/radon-csars.git');
+	
+	// require('simple-git/promise')(repoPath).init();
+	// console.info("Dopo init");
+	// require('simple-git/promise')(repoPath).add(fileName);
+	// console.info("Dopo add");
+	// require('simple-git/promise')(repoPath).commit("Commit of csar");
+	// console.info("Dopo commit");
+	// require('simple-git/promise')(repoPath).addRemote('origin', 'https://${userName}:${password}@github.com/radon-h2020/radon-csars.git');
+	// console.info("Dopo remote");
+	// require('simple-git/promise')(repoPath).push('origin', 'master');
+	// simpleGitPromise
+    //  .init()
+    //  .add('./*')
+    //  .commit("first commit!")
+    //  .addRemote('origin', 'https://${userName}:${password}@github.com/radon-h2020/radon-csars.git')
+    //  .push('origin', 'master');
+		vscode.window.showInformationMessage('Deployment process started: '+fileName);
 	});
 
 	let disposableDeployStatus = vscode.commands.registerCommand('radon-menu.deployStatus', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.env.openExternal(vscode.Uri.parse("https://www.google.it/"));
+		vscode.env.openExternal(vscode.Uri.parse("http://operations.radon-h2020.eu:8080/"));
 		vscode.window.showInformationMessage('Opened deployment page');
 	});
 
@@ -70,12 +108,57 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.env.openExternal(vscode.Uri.parse("https://www.google.it/"));
+		vscode.env.openExternal(vscode.Uri.parse("https://github.com/radon-h2020/radon-methodology#table-of-contents"));
 		vscode.window.showInformationMessage('Opened RADON Help page');
 	});
 
 	context.subscriptions.push(disposableGMT,disposableVT,disposableCTT,disposableDT,disposableDPT,disposableHelp);
 }
+
+function pushCsarToGitHubRepository(csarPath:string, repo:string) {
+
+	// User name and password of your GitHub
+	const userName = 'radon-dev';
+	const password = 'radon-h2020';
+
+	const simpleGitPromise = require('simple-git/promise')("projects/radon-csars");
+	
+	// Set up GitHub url like this so no manual entry of user pass needed
+	const gitHubUrl = 'https://${userName}:${password}@${repo}';
+	
+	// add local git config like username and email
+	//simpleGit.addConfig('user.email','balvinder294@gmail.com');
+	//simpleGit.addConfig('user.name','Balvinder Singh');
+
+	// Add remore repo url as origin to repo
+	simpleGitPromise.addRemote('origin',gitHubUrl);
+
+	// Add all files for commit
+  	simpleGitPromise.add(csarPath)
+    .then(
+       (addSuccess:any) => {
+          console.log(addSuccess);
+       }, (failedAd:any) => {
+          console.log('adding csar file failed');
+    });
+
+	// Commit files as Initial Commit
+	simpleGitPromise.commit('Commit csar by simplegit')
+   	.then(
+      (successCommit:any) => {
+        console.log(successCommit);
+     }, (failed:any) => {
+        console.log('failed commmit csar');
+	 });
+	 
+	// Finally push to online repository
+	simpleGitPromise.push('origin','master')
+	.then((success:any) => {
+		console.log('repo successfully pushed csar');
+	},(failed:any)=> {
+		console.log('repo push csar failed');
+	});
+  }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
